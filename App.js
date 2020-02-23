a2d755e88e45de7fc024e06347074bdabbbcb102
Jinhyehye <jinhye.garage@gmail.com>
2020-02-23 14:05:42 -0800

    Search box

--- App.js
+++ App.js
@@ -1,19 +1,66 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, Button, TextInput, ScrollView, FlatList } from 'react-native';
 
 export default function App() {
+  const [enteredName, setEnteredName] = useState('');
+
+  const [enteredNameHistory, setEnteredNameHistory] = useState([]);
+
+  const nameInputHandler = (enteredText) => {
+    setEnteredName(enteredText);
+  }
+
+  const nameSearchHandler = () => {
+    setEnteredNameHistory(
+      currentName => [...currentName,
+      { id: Math.random().toString, value: enteredName }
+      ]);
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Hello World!</Text>
+    <View style={styles.screen}>
+      <View style={styles.inputContainer}>
+        <TextInput
+          placeholder="Search"
+          style={styles.input}
+          onChangeText={nameInputHandler}
+          value={enteredName}
+        />
+        <Button title="Go!" onPress={nameSearchHandler} />
+      </View>
+      <FlatList 
+        keyExtractor={(item, index)=>item.id}
+        data={enteredNameHistory} 
+        renderItem={itemData => (
+        <View style={styles.searchHistory}>
+          <Text>{itemData.item.value}</Text>
+        </View>
+        )}
+      />
+
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  screen: {
+    padding: 50
   },
+  inputContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center'
+  },
+  input: {
+    width: '90%',
+    borderBottomColor: 'grey',
+    borderBottomWidth: 1,
+    padding: 10
+  },
+  searchHistory: {
+    padding: 10,
+    marginVertical: 10,
+    backgroundColor: '#ccc'
+  }
+
 });
